3485cdc8e52359b74ba1f564b6f76bf0af6ed428
Frederik <58188038+Skinkestakken@users.noreply.github.com>
2020-11-03 02:04:06 +0100

    goodnight

---
+++ server/src/models/crmModel.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const QuestionSchema = new Schema({
+    title: {
+        type: String,
+        required: 'Enter a title'
+    },
+    text: {
+        type: String,
+        required: 'Enter a text'
+    },
+    date_created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// CONTINUE FROM CREATE POST ENDPOINT ON LINKEDIN LEARNING
